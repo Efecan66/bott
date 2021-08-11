d1e3389dd951409a6d9a1e2824ee5c4844067159
Efecan66 <88770915+Efecan66@users.noreply.github.com>
2021-08-11 12:11:05 +0300

    Add files via upload

---
+++ ayarlar.js
@@ -0,0 +1,13 @@
+const ayarlar = {
+  TOKEN: ""
+  owner: "626465919291621377",
+  prefix: ".",
+
+};
+
+module.exports = ayarlar;
+
+//BOT TOKENINIZI EGER BOT GLITCHDE KALACAK
+//ISE .ENV DOSYASINA
+//EGER VDS YE KURACAK ISENIZ YUKARIYA 1. SATIRA YAZIN4
+
